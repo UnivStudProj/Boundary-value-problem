acbef85712c7bf10066f1d5f21583e4bbb573b00
muivproject <95047127+muivproject@users.noreply.github.com>
2021-12-29 14:17:36 +0300

    some structure

--- idk.py
+++ idk.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import logging
 
 # x = np.linspace(-3, 3)
 # plt.plot(x, x**2)
 
 # plt.show()
+
+logging.basicConfig(filename="sample.log", format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 D = 0.06
 c = 0.5
@@ -13,9 +16,9 @@ u_c = 0
 l = 10
 T = 40
 
-x_dots = np.arange(0, l, 0.1)
+x_dots = np.arange(0, l, 0.5)
 t_dots = np.arange(0.8, T, 4)
-psi_of_x = [u_c + (1 + np.cos((np.pi * i) / l)) for i in x_dots]
+psi_of_x = [u_c + (1 + np.cos((np.pi * x) / l)) for x in x_dots]
 
 I = len(x_dots) # 100
 K = len(t_dots) # 10
@@ -25,57 +28,83 @@ cube_a = D / c
 x = [i * h_x for i in x_dots]
 t = [k * h_t for k in t_dots]
 sigma = (h_t * cube_a) / (2 * (h_x ** 2))
+dots_bottom = {}
+dots_top = dict([('u' + str(i), psi_of_x[i]) for i in range(len(psi_of_x))])
 
 
-def CrankNicolson_args(i, k):
-    global line_b, line_t
-    # bottom dots
-    line_b = {
-        'u_curr' : [None, x[i], t[k]],
-        'u_next' : [None, x[i + 1], t[k]],
-        'u_prev' : [None, x[i - 1], t[k]]
-    } 
-    # top dots
-    line_t = {
-        'u_curr' : [None, x[i], t[k + 1]],
-        'u_next' : [None, x[i + 1], t[k + 1]],
-        'u_prev' : [None, x[i - 1], t[k + 1]]
-    }
+# Building Crank-Nicolson scheme
+def CrankNicolson_args(i, k=0):
+    global dots_bottom 
     # Sweep method
     if i == 0:
-        left_side = -2 * sigma * dot(1, 1) + (1 + 2 * sigma) * dot(1, 0)
-        right_side = dot(0, 0) + 2 * sigma * (dot(0, 1) - dot(0, 0))
-    elif i >= 1 and i <= I -1:
-        left_side = -sigma * dot(1, 1) + (1 + 2 * sigma) * dot(1, 0) - sigma * dot(1, 2)
-        right_side = dot(0, 0) + sigma * (dot(0, 1) - 2 * dot(0, 0) + dot(0, 2)) 
-    elif i == I:
-        left_side = (1 + 2 * sigma + 2 * sigma * H * h_x) * dot(1, 0) - 2 * sigma * dot(1, 2)
-        right_side = dot(0, 0) + 2 * sigma * ((-1 - H * h_x) * dot(0, 0) + dot(0, 2))
-        
-CrankNicolson_args(0, 5)
+        # Matrix elements
+        a_i = 0
+        b_i = 1 + 2 * sigma
+        c_i = -2 * sigma
+        # Run coefficients
+        p_next = (2 * sigma) / (1 + 2 * sigma)
+        q_next = getPrevDot(a_i, b_i, c_i, i) / (1 + 2 * sigma)
+        # Setting coeffs for the next run 
+        setCoeff_p(p_next)
+        setCoeff_q(q_next)
+        u_0 = -(c_i / b_i) * CrankNicolson_args(i + 1) + getPrevDot(a_i, b_i, c_i, i) / b_i
+        dots_bottom['u' + str(i)] = [u_0, (x[i], t[k])]
+    elif i >= 1 and i <= I - 2:
+        # Matrix elements
+        a_i = -sigma
+        b_i = 1 + 2 * sigma
+        c_i = -sigma
+        # Run coefficients
+        p_next = -sigma * (1 + 2 * sigma - sigma * p_curr)
+        q_next = (getPrevDot(a_i, b_i, c_i, i) + sigma * q_curr) / (1 + 2 * sigma - sigma * p_curr)
+        # Setting coeffs for the next run 
+        setCoeff_p(p_next)
+        setCoeff_q(q_next)
+        u_i = -c_i * (b_i + p_next * a_i) * CrankNicolson_args(i + 1)
+        dots_bottom['u' + str(i)] = [u_i, (x[i], t[k])]
+        return u_i
+    elif i == I - 1:
+        # Matrix elements
+        a_i = -2 * sigma
+        b_i = 1 + 2 * sigma + 2 * sigma * H * h_x
+        c_i = 0 
+        # Run coefficients
+        p_next = 0
+        q_next = (getPrevDot(a_i, b_i, c_i, i) - a_i * q_curr) / (1 + 2 * sigma - 2 * sigma * H * h_x - 2 * sigma * p_curr)
+        # Setting coeffs for the next run 
+        setCoeff_p(p_next)
+        setCoeff_q(q_next)
+        u_I = (getPrevDot(a_i, b_i, c_i, i) - a_i * q_next) / (b_i + a_i * p_next)
+        dots_bottom['u' + str(i)] = [u_I, (x[i], t[k])]
+        return u_I
 
+   
+# Set the "p" run coeff 
+def setCoeff_p(num):
+    global p_curr
+    p_curr = num
+   
 
-# For text reduction
-def dot(line, pos):
-    if line == 0:
-        l = line_b
-    else:
-        l = line_t
-    # Searching dot positon in the dicitionary
-    for d in range(len(l)):
-        if d == pos:
-            return list(line_b.values())[d][0]
+# Set the "q" run coeff 
+def setCoeff_q(num):
+    global q_curr
+    q_curr = num
     
-
-print(dot(0, 1))
     
-def CrankNicolson_scheme():
-    pass  
-    
+# Returning the previous (already known) dot    
+def getPrevDot(a, b, c, i):
+    part1 = 0 if a == 0 else a * dots_top['u' + str(i - 1)] 
+    part2 = b * dots_top['u' + str(i)] 
+    part3 = 0 if c == 0 else c * dots_top['u' + str(i + 1)]
+    return part1 + part2 + part3
 
-# Substance concentration
 
-# plt.plot(x_dots, psi_of_x)
+CrankNicolson_args(0, 2)
+l = [list(dots_bottom.values())[i][0] for i in range(len(dots_bottom))]
+print(dots_bottom)
 
-# plt.show()
+plt.plot(x_dots, l)
+
+plt.grid()
+plt.show()
     
--- test.py
+++ test.py
@@ -1,11 +1,6 @@
-x = [0, 1 ,2]
-t = [3, 4, 5]
-i = 1
-k = i
-line_b = {
-    'u_curr' : [None, x[i], t[k]],
-    'u_next' : [None, x[i + 1], t[k]],
-    'u_prev' : [None, x[i - 1], t[k]]
-} 
-for k, v, k1  in line_b.items(), line_b:
-     print(k1)
+I = 255
+
+x = 999 if I == 0 else 666
+y = True if I == 255 else False
+
+print(x, y)
