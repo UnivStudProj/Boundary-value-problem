fbb77a3cbca816caf24c31c3facd6b3a3b7d7138
muivproject <95047127+muivproject@users.noreply.github.com>
2021-12-29 22:35:19 +0300

    using psi values

--- idk.py
+++ idk.py
@@ -16,7 +16,10 @@ u_c = 0
 l = 10
 T = 40
 
-x_dots = np.arange(0, l, 0.5)
+x_start = 0
+x_step = 1.0
+x_dots = np.arange(x_start, l, x_step)
+
 t_start = 0.80
 t_step = 4
 t_dots = np.arange(t_start, T, t_step)
@@ -30,11 +33,12 @@ cube_a = D / c
 x = [i * h_x for i in x_dots]
 t = [k * h_t for k in t_dots]
 sigma = (h_t * cube_a) / (2 * (h_x ** 2))
-plots = {'u(x, 0)' : psi_of_x}
+start_dots = {f'u({x_start + x_step * i}, t)' : psi_of_x[i] for i in range(len(psi_of_x))}
+new_dots = {}
 
 
 # Building Crank-Nicolson scheme
-def CrankNicolson_args(i, k=0):
+def CrankNicolson_args(i):
     # Sweep method
     b = 1 + 2 * sigma
     if i == 0:
@@ -43,41 +47,36 @@ def CrankNicolson_args(i, k=0):
         c = -2 * sigma
         # Run coefficients
         p_next = (2 * sigma) / (1 + 2 * sigma)
-        q_next = [dot / (1 + 2 * sigma) for dot in getPrevDot(a, b, c, i)]
+        q_next = getPrevLine(a, b, c, i) / (1 + 2 * sigma)
         # Setting coeffs for the next run 
         setCoeff_p(p_next)
         setCoeff_q(q_next)
-        u_0 = [-(c / b) * CrankNicolson_args(i + 1) + dot / b for dot in getPrevDot(a, b, c, i)]
-        plots[f'u(x, {t_start + t_step})'] = u_0
+        u_0 = p_next * CrankNicolson_args(i + 1) + q_next
+        new_dots[f'u({x_start}, t)'] = u_0
     elif i >= 1 and i <= I - 2:
         # Matrix elements
         a = -sigma
         c = -sigma
+        u_i = -c * (b + p_curr * a) 
         # Run coefficients
         p_next = -sigma * (1 + 2 * sigma - sigma * p_curr)
-        q_next = [(dot + sigma * q_curr) / (1 + 2 * sigma - sigma * p_curr) for dot in getPrevDot(a, b, c, i)]
+        q_next = (getPrevLine(a, b, c, i) + sigma * q_curr) / (1 + 2 * sigma - sigma * p_curr)
         # Setting coeffs for the next run 
         setCoeff_p(p_next)
         setCoeff_q(q_next)
-        u_i = -c * (b + p_next * a) * CrankNicolson_args(i + 1)
-        plots[f'u(x, {t_start + t_step * i})'] = u_i
+        u_i *= CrankNicolson_args(i + 1)
+        new_dots[f'u({x_start + x_step * i}, t)'] = u_i
         return u_i
     elif i == I - 1:
         # Matrix elements
         a = -2 * sigma
         b += 2 * sigma * H * h_x
         c = 0 
-        # Run coefficients
-        p_next = 0
-        q_next = [(dot - a * q_curr) / (1 + 2 * sigma - 2 * sigma * H * h_x - 2 * sigma * p_curr) for dot in getPrevDot(a, b, c, i)]
-        # Setting coeffs for the next run 
-        setCoeff_p(p_next)
-        setCoeff_q(q_next)
-        u_I = [(dot - a * q_el) / (b + a * p_next) for dot, q_el in zip(getPrevDot(a, b, c, i), q_next)]
-        plots[f'u(x, {t_start + t_step * i})'] = u_I 
+        u_I = (getPrevLine(a, b, c, i) - a * q_curr) / (b + a * p_curr)
+        new_dots[f'u({x_start + x_step * i}, t)'] = u_I 
         return u_I
 
-   
+
 # Set the "p" run coeff 
 def setCoeff_p(num):
     global p_curr
@@ -90,18 +89,20 @@ def setCoeff_q(num):
     q_curr = num
     
     
-# Returning the previous (already known) dot    
-def getPrevDot(a, b, c, i):
-    part1 = 0 if a == 0 else [a * dot for dot in plots[f'u(x, {t_start + t_step * (i - 1)})']] 
-    part2 = [b * dot for dot in plots[f'u(x, {t_start + t_step * i})']] 
-    part3 = 0 if c == 0 else [c * dot for dot in plots[f'u(x, {t_start + t_step * (i + 1)})']]
-    return [el1 + el2 + el3 for el1, el2, el3 in zip(part1, part2, part3)]
+# Returning the previous line    
+def getPrevLine(a, b, c, i):
+    part1 = 0 if a == 0 else a * start_dots[f'u({x_start + x_step * (i - 1)}, t)']
+    part2 = b * start_dots[f'u({x_start + x_step * i}, t)'] 
+    part3 = 0 if c == 0 else c * start_dots[f'u({x_start + x_step * (i + 1)}, t)']
+    return part1 + part2 + part3
 
 
 CrankNicolson_args(0)
 
-plt.plot(x_dots, l)
+l = [v for v in list(new_dots.values())]
+print(l)
+# plt.plot(x_dots, l)
 
-plt.grid()
-plt.show()
+# plt.grid()
+# plt.show()
     
